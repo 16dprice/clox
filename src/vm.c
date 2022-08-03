cdcce5204ea6713fd05735c2e04c7ba9f1c2fc40
DJ <donald.price077@topper.wku.edu>
2022-08-03 09:20:55 -0500

    15.1.2:: execution tracing

--- src/vm.c
+++ src/vm.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "vm.h"
 #include "chunk.h"
+#include "debug.h"
 
 #include <stdio.h>
 
@@ -19,6 +20,10 @@ static InterpretResult run() {
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for(;;) {
+    #ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    #endif
+
     uint8_t instruction;
 
     switch (instruction = READ_BYTE()) {
