3d8de6f888c0c6bfd065643fccb7fc5d767951b8
DJ <djpric91@gmail.com>
2022-08-07 13:52:06 -0500

    bugfix: fprintf missing argument

--- main.c
+++ main.c
@@ -35,13 +35,13 @@ static char* readFile(const char* path) {
 
   char* buffer = (char*) malloc(fileSize + 1); // file size plus 1 because we add a null byte
   if(buffer == NULL) {
-    fprintf(stderr, "Not enough memory to read \"%s\".\n");
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
     exit(74);
   }
 
   size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
   if(bytesRead < fileSize) {
-    fprintf(stderr, "Could not read file \"%s\".\n");
+    fprintf(stderr, "Could not read file \"%s\".\n", path);
     exit(74);
   }
 
@@ -61,45 +61,45 @@ static void runFile(const char* path) {
 }
 
 int main(int argc, const char* argv[]) {
-  initVM();
+  // initVM();
 
-  if(argc == 1) {
-    repl();
-  } else if(argc == 2) {
-    runFile(argv[1]);
-  } else {
-    fprintf(stderr, "Usage: clox [path]\n");
-    exit(64);
-  }
+  // if(argc == 1) {
+  //   repl();
+  // } else if(argc == 2) {
+  //   runFile(argv[1]);
+  // } else {
+  //   fprintf(stderr, "Usage: clox [path]\n");
+  //   exit(64);
+  // }
 
-  Chunk chunk;
-  initChunk(&chunk);
+  // Chunk chunk;
+  // initChunk(&chunk);
 
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  // int constant = addConstant(&chunk, 1.2);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
 
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  // constant = addConstant(&chunk, 3.4);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_ADD, 123);
+  // writeChunk(&chunk, OP_ADD, 123);
 
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  // constant = addConstant(&chunk, 5.6);
+  // writeChunk(&chunk, OP_CONSTANT, 123);
+  // writeChunk(&chunk, constant, 123);
 
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
+  // writeChunk(&chunk, OP_DIVIDE, 123);
+  // writeChunk(&chunk, OP_NEGATE, 123);
 
-  writeChunk(&chunk, OP_RETURN, 123);
+  // writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "Test Chunk");
+  // disassembleChunk(&chunk, "Test Chunk");
 
-  interpret(&chunk);
+  // interpret(&chunk);
 
-  freeVM();
-  freeChunk(&chunk);
+  // freeVM();
+  // freeChunk(&chunk);
 
   return 0;
 }
